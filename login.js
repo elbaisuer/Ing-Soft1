68d5ca579e1121e94207585ff6f00e2eac3a4cce
elbaisuer <60907498+elbaisuer@users.noreply.github.com>
2023-10-21 09:43:32 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,17 @@
+function loguear(){
+
+let user=document.getElementById("usuario").value;
+let pass=document.getElementById("clave").value;
+
+if(user==  &&  pass== ){
+
+    window.location="bienvenida.html";
+
+}
+
+else{
+
+    alert("Datos Incorrectos");
+}
+
+}
